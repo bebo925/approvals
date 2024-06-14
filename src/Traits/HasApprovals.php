0efd2a744eb0757084ab68e29a95755b9dad864e
Ryan McQuerry <bebo925@gmail.com>
2024-06-14 11:18:47 -0400

    Update HasApprovals.php

--- src/Traits/HasApprovals.php
+++ src/Traits/HasApprovals.php
@@ -3,6 +3,7 @@
 namespace bebo925\Approvals\Traits;
 
 use bebo925\Approvals\Approval;
+use bebo925\Approvals\ApprovalStep;
 use bebo925\Approvals\Enums\ApprovalStatus;
 
 trait HasApprovals
@@ -10,6 +11,19 @@ trait HasApprovals
     public function approvals()
     {
         return $this->morphMany(Approval::class, 'approvable');
+    }
+
+    public function generateApprovalSteps()
+    {
+        //Todo: check for existing approvals and do something about it
+        ApprovalStep::where('approvable_class', self::class)
+            ->orderBy('order')
+            ->get()
+            ->each(function (ApprovalStep $approvalStep) {
+                $this->approvals()->create([
+                    'approval_step_id' => $approvalStep->id,
+                ]);
+            });
     }
 
     public function isApproved()
